4205c84735a3fb09655cb5ea6ecba50714f14959
JP Wright <jp@contentful.com>
2017-01-31 17:40:28 +0100

    Add detailed comments to ViewController file

--- Boilerplate/ViewController.swift
+++ Boilerplate/ViewController.swift
@@ -12,18 +12,12 @@ import Interstellar
 
 class ViewController: UIViewController {
 
-    let client: Client
-
-    // MARK: Initialization
-
-    init() {
-        self.client = Client(spaceIdentifier: "cfexampleapi", accessToken: "b4c0n73n7fu1")
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    /**
+     * We must initialize a Contentful.Client object to infterface with the Contentful CDA.
+     * In this case, it's an instance variable on our viewController. Copy paste your space id,
+     * and your CDA API key (access token) to replace the passed in variables below.
+     */
+    let client: Client = Client(spaceIdentifier: "cfexampleapi", accessToken: "b4c0n73n7fu1")
 
     // MARK: UIViewController
     
@@ -31,34 +25,44 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
 
-        // Fetch space
+        /**
+         * All of the Client methods to retrieve content from the API are prefixed with `fetch`
+         * They use asynchronous callbacks to handle the responses. The response has a Result enum
+         * which encapsulates the returned value if the request was successful, or the error if the 
+         * request failed.
+         * Don't forget to delegate to the main thread if you are going to update the UI with the content!
+         */
         client.fetchSpace { [weak self] (result: Result<Space>) in
 
             // This callback is where your network response will be handled
             // If you're going to update the UI...make sure to delegate back to the main thread.
             switch result {
             case .success(let space):
+                print("=================Printing space=================")
                 print("Fetched space with name \(space.name)")
+                print("==============Done Printing space===============")
             case .error(let error):
                 self?.handle(error: error)
             }
         }
 
-        // Fetch entries
+        /**
+         * Similar to `fetchSpace` fetchEntries returns a Result, but this time containing a Contentful
+         * Array of Entry objects.
+         */
         client.fetchEntries(matching: ["limit": 5]) { [weak self] (result: Result<Contentful.Array<Entry>>) in
             switch result {
             case .success(let entries):
-                print("Printing Entries")
+                print("=================Printing Entries=================")
                 entries.items.forEach { entry in
                     print(entry.identifier)
                 }
-
+                print("==============Done Printing Entries===============")
             case .error(let error):
                 self?.handle(error: error)
             }
         }
     }
-
 
     // MARK: Private
 
