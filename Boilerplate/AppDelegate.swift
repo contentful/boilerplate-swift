2d6edd3abd91f9fa1fec92ca0367b25536cd0461
JP Wright <jp@contentful.com>
2017-01-31 14:23:38 +0100

    Put Contentful.Client code in ViewController and print out responses

--- Boilerplate/AppDelegate.swift
+++ Boilerplate/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         let viewController = ViewController()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
 
--- Boilerplate/ViewController.swift
+++ Boilerplate/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Contentful
+import Interstellar
 
 class ViewController: UIViewController {
 
@@ -29,6 +30,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
+        // Fetch space
+        client.fetchSpace { [weak self] (result: Result<Space>) in
+
+            // This callback is where your network response will be handled
+            // If you're going to update the UI...make sure to delegate back to the main thread.
+            switch result {
+            case .success(let space):
+                print("Fetched space with name \(space.name)")
+            case .error(let error):
+                self?.handle(error: error)
+            }
+        }
+
+        // Fetch entries
+        client.fetchEntries(matching: ["limit": 5]) { [weak self] (result: Result<Contentful.Array<Entry>>) in
+            switch result {
+            case .success(let entries):
+                print("Printing Entries")
+                entries.items.forEach { entry in
+                    print(entry.identifier)
+                }
+
+            case .error(let error):
+                self?.handle(error: error)
+            }
+        }
     }
 
+
+    // MARK: Private
+
+    func handle(error: Error) {
+        print("Uh oh, something went wrong. You can do what you want with this \(error)")
+    }
 }
